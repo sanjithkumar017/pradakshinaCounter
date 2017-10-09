5c986fdac78b109b11f7f5e7ecf11df6800e3fe7
sanjeeth.kumar <sanjeeth.kumar@6dtech.co.in>
2017-10-09 19:43:30 +0530

    Updated TO-DO

--- App/components/Counter.js
+++ App/components/Counter.js
@@ -19,6 +19,14 @@ import styles from '../styles/styles.js';
 //We need to create seperate components
 //We need to reuse the components
 
+//Spend 5 days on testing
+//Unit testing
+//All types of testing
+
+//Save 4 dates into separate Async Names
+//When componentWillMount is called, load these dates onto Redux state
+//Set each date on complete of 41
+////We need to be intelligent enough to set the correct date
 
 //We need to store the start date and 4 dots and the end date in AsyncStorage.
 //Save it as 4 objects with date as current day number, like 2nd, 17th and so on.
